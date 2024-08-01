2f48481156568231d2df9e5859deb960a3d81c96
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-01 15:17:59 +0500

    Goal keeper is done

---
+++ GoalKeeper.java
@@ -0,0 +1,17 @@
+package Interface;
+
+public class GoalKeeper  extends Player{
+
+    private int countOfSaves;
+
+    public GoalKeeper(){
+        super.name ="Abubakr";
+        this.countOfSaves = 9;
+
+    }
+
+    @Override
+    public void getInfo(){
+        super.getInfo();
+    }
+}
---
+++ Setter.java
@@ -0,0 +1,23 @@
+package Interface;
+
+public class Setter extends Player {
+
+    private int countOfSet;
+
+    public Setter(){
+        this.countOfSet = 7;
+
+    }
+
+    @Override
+    public void getInfo(){
+        super.getInfo();
+
+    }
+
+    @Override
+    public void getTeamName(){
+        super.getTeamName();
+
+    }
+}
